10ed191b180622f26ed0a485c9d012f807dffbf3
lsaturn <lsaturn@gmail.com>
2015-07-02 17:52:51 +0800

    新增AliyunOss的支持与测试，只完成了代码桩子

---
+++ src/NAppUpdate.Framework/Sources/AliyunOssSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NAppUpdate.Framework.Common;
+using NAppUpdate.Framework.Utils;
+using System.Net;
+using System.IO;
+
+namespace NAppUpdate.Framework.Sources
+{
+	public class AliyunOssSource : IUpdateSource
+	{
+		public AliyunOssSource(string accessKeyId, string accessKeySecret, string bucketName
+            , string sourceRoot)
+		{
+		}
+
+		#region IUpdateSource Members
+
+	    public string GetUpdatesFeed()
+	    {
+			return "";
+		}
+
+		public bool GetData(string url, string baseUrl, Action<UpdateProgressInfo> onProgress, ref string tempLocation)
+		{
+			return true;
+		}
+
+		#endregion
+	}
+}
---
+++ src/NAppUpdate.Tests/Integration/AliyunOssSourceTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NAppUpdate.Framework.Sources;
+
+namespace NAppUpdate.Tests.Integration
+{
+	[TestClass]
+	public class AliyunOssSourceTests
+    {
+		[TestMethod]
+		public void can_download_ansi_feed()
+		{
+            var aos = new AliyunOssSource("QuBc7LxHY0Ov7xL6", "i57W4W7v6di74ObX4LZSa8NbejTF2d", "2ciyuan-test", "NetterClient");
+// 			const string expected = "NHibernate.Profiler-Build-";
+// 
+// 			var ws = new SimpleWebSource("http://builds.hibernatingrhinos.com/latest/nhprof");
+// 			var str = ws.GetUpdatesFeed();
+// 
+// 			Assert.AreEqual(expected, str.Substring(0, expected.Length));
+		}
+	}
+}
